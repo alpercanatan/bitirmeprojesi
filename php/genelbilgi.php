6aa941671e432246f73f64f3b9e0aad2804b716d
stronger92 <stronger_1992@hotmail.com>
2014-11-17 13:01:53 +0200

    anasayfadaki gelen genelbilgileri şekillendirdim

--- php/genelbilgi.php
+++ php/genelbilgi.php
@@ -13,16 +13,16 @@ $sorgu4 = $baglanti->query("SELECT COUNT(ID) FROM `urunler`");
 
 
 	$mesaj = $sorgu->fetch_assoc();
-	echo "<b>Firmalar:</b> " . $mesaj['COUNT(ID)'] . "<br>";
+	echo '<div class="group"><h2>FİRMALAR</h2><ul><li id="li_server_info">' . '<b>Kayıtlı Firma Sayısı:</b> '. $mesaj['COUNT(ID)'] . '</li></ul></div>';
 	
 	$mesaj2 = $sorgu2->fetch_assoc();
-	echo "<b>Kullanıcılar:</b> " . $mesaj2['COUNT(ID)'] . "<br>";
+	echo '<div class="group"><h2>KULLANICILAR</h2><ul><li id="li_server_info">' . '<b>Kayıtlı Kullanıcı Sayısı:</b> ' . $mesaj2['COUNT(ID)'] . '</li></ul></div>';
 
 	$mesaj3 = $sorgu3->fetch_assoc();
-	echo "<b>Teklifler:</b> " . $mesaj3['COUNT(ID)'] . "<br>";
+	echo '<div class="group"><h2>TEKLİFLER</h2><ul><li id="li_server_info">' . '<b>Kayıtlı Teklif Sayısı:</b> ' . $mesaj3['COUNT(ID)'] . '</li></ul></div>';
 
 	$mesaj4 = $sorgu4->fetch_assoc();
-	echo "<b>Ürünler:</b> " . $mesaj4['COUNT(ID)'];
+	echo '<div class="group"><h2>ÜRÜNLER</h2><ul><li id="li_server_info">' . '<b>Kayıtlı Ürün Sayısı:</b> ' . $mesaj4['COUNT(ID)'] . '</li></ul></div>';
 
 
 
